243782519fa4ecf3373a92f974a07df7834e8786
loke <lokendar384@gmail.com>
2025-06-26 22:09:46 +0545

    urls created

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
+from fastapi import APIRouter
 from . import views
 
+router = APIRouter()
+
+# URL patterns equivalent
 urlpatterns = [
-    path('', views.home, name='home'),
+    {"path": "/api/users/", "methods": ["GET", "POST"], "handler": "users"},
+    {"path": "/api/users/{user_id}", "methods": ["GET", "PUT", "DELETE"], "handler": "user_detail"},
+    {"path": "/api/posts/", "methods": ["GET", "POST"], "handler": "posts"},
+    {"path": "/api/posts/{post_id}", "methods": ["GET", "PUT", "DELETE"], "handler": "post_detail"},
+    {"path": "/api/categories/", "methods": ["GET", "POST"], "handler": "categories"},
+    {"path": "/admin/", "methods": ["GET"], "handler": "admin_panel"},
 ]
